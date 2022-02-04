27122ae0e9a92bf50023e40e27a218aa6f744cb6
Takuto Yanagida <yanagida@stxst.com>
2022-02-04 16:13:35 +0900

    Move functions to another package

--- src/utility.php
+++ src/utility.php
@@ -1,50 +1,13 @@
 <?php
 /**
- * Miscellaneous
+ * Utilities
  *
  * @package Wpinc Sys
  * @author Takuto Yanagida
- * @version 2022-02-03
+ * @version 2022-02-04
  */
 
 namespace wpinc\sys;
-
-/**
- * Checks current post type.
- *
- * @param string $post_type Post type.
- * @return bool True if the current post type is $post_type.
- */
-function is_post_type( string $post_type ): bool {
-	$id_g = $_GET['post'] ?? null;  // phpcs:ignore
-	$id_p = $_POST['post_ID'] ?? null;  // phpcs:ignore
-	if ( ! $id_g && ! $id_p ) {
-		return false;
-	}
-	$p = get_post( intval( $id_g ? $id_g : $id_p ) );
-	if ( $p ) {
-		$pt = $p->post_type;
-	} else {
-		$pt = $_GET['post_type'] ?? '';  // phpcs:ignore
-	}
-	return $post_type === $pt;
-}
-
-/**
- * Retrieves post type title.
- */
-function get_post_type_title() {
-	$post_type = get_query_var( 'post_type' );
-	if ( is_array( $post_type ) ) {
-		$post_type = reset( $post_type );
-	}
-	$post_type_obj = get_post_type_object( $post_type );
-	return apply_filters( 'post_type_archive_title', $post_type_obj->labels->name, $post_type );
-}
-
-
-// -----------------------------------------------------------------------------
-
 
 /**
  * Gets current URL.
