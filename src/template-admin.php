b028ac100ac3c74628488321f7793e719c1694d6
Takuto Yanagida <yanagida@stxst.com>
2023-06-03 17:12:18 +0900

    Fix typo

--- src/template-admin.php
+++ src/template-admin.php
@@ -4,7 +4,7 @@
  *
  * @package Wpinc Sys
  * @author Takuto Yanagida
- * @version 2022-10-11
+ * @version 2023-06-03
  */
 
 namespace wpinc\sys\template_admin;
@@ -20,12 +20,12 @@ function activate( string $function_name = 'setup_template_admin' ): void {
 	if ( ! is_admin() ) {
 		return;
 	}
-	$post_fixes = array( '--admin.php', '_admin.php' );
+	$suffixes = array( '--admin.php', '_admin.php' );
 
 	add_action(
 		'admin_menu',
-		function () use ( $post_fixes, $function_name ) {
-			_cb_admin_menu__template_admin( $post_fixes, $function_name );
+		function () use ( $suffixes, $function_name ) {
+			_cb_admin_menu__template_admin( $suffixes, $function_name );
 		}
 	);
 }
@@ -33,31 +33,31 @@ function activate( string $function_name = 'setup_template_admin' ): void {
 /**
  * Callback function for 'admin_menu' action.
  *
- * @param array  $post_fixes    Post-fixes of file names of template admin files.
+ * @param array  $suffixes      Post-fixes of file names of template admin files.
  * @param string $function_name Function name.
  */
-function _cb_admin_menu__template_admin( array $post_fixes, string $function_name ): void {
+function _cb_admin_menu__template_admin( array $suffixes, string $function_name ): void {
 	$post_id = \wpinc\get_admin_post_id();
 
 	$pt = get_post_meta( $post_id, '_wp_page_template', true );
 	if ( ! empty( $pt ) && 'default' !== $pt ) {
-		foreach ( $post_fixes as $post_fix ) {
-			if ( _load_page_template_admin( $post_id, $pt, $post_fix, $function_name ) ) {
+		foreach ( $suffixes as $sf ) {
+			if ( _load_page_template_admin( $post_id, $pt, $sf, $function_name ) ) {
 				return;
 			}
 		}
 	}
 	if ( _is_page_on_front( $post_id ) ) {
-		foreach ( $post_fixes as $post_fix ) {
-			if ( _load_page_template_admin( $post_id, 'front-page.php', $post_fix, $function_name ) ) {
+		foreach ( $suffixes as $sf ) {
+			if ( _load_page_template_admin( $post_id, 'front-page.php', $sf, $function_name ) ) {
 				return;
 			}
 		}
 	}
 	$post_type = \wpinc\get_admin_post_type();
 	if ( ! empty( $post_type ) ) {
-		foreach ( $post_fixes as $post_fix ) {
-			if ( _load_page_template_admin( $post_id, $post_type . '.php', $post_fix, $function_name ) ) {
+		foreach ( $suffixes as $sf ) {
+			if ( _load_page_template_admin( $post_id, $post_type . '.php', $sf, $function_name ) ) {
 				return;
 			}
 		}
@@ -71,11 +71,11 @@ function _cb_admin_menu__template_admin( array $post_fixes, string $function_nam
  *
  * @param int    $post_id       The post ID.
  * @param string $path          The path to the page template admin.
- * @param string $post_fix      Postfix of the file name.
+ * @param string $suffix        Postfix of the file name.
  * @param string $function_name Function name for admin.
  */
-function _load_page_template_admin( int $post_id, string $path, string $post_fix, string $function_name ) {
-	$path = str_replace( '.php', $post_fix, $path );
+function _load_page_template_admin( int $post_id, string $path, string $suffix, string $function_name ) {
+	$path = str_replace( '.php', $suffix, $path );
 	$path = get_parent_theme_file_path( $path );
 
 	if ( file_exists( $path ) ) {
